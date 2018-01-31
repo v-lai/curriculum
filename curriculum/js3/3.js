5c40b917f235549ae01bebc7a5c278468c923b0a
Alberto Lopez <alopez@garagescript.org>
2018-01-31 14:04:10 -0800

    repeating elements

--- curriculum/js3/3.js
+++ curriculum/js3/3.js
@@ -4,28 +4,8 @@
  * @return {array}
  */
 
-const createObject = (arr, obj={}, i=0)=>{
-  if(i === arr.length){
-    return obj;
-  }
-  if(!obj[arr[i]]){
-    obj[arr[i]]=0;
-  }
-  obj[arr[i]]+=1;
-  return createObject(arr, obj, i+1);
-}
-const timesRepeat = (obj, keys, i=0, arr1=[])=>{
-  if(i === keys.length){
-    return arr1;
-  }
-  if(obj[keys[i]]>1){
-    arr1.push(parseInt(keys[i]));
-  }
-  return timesRepeat(obj, keys, i+1, arr1);
-}
-const solution = (arr)=>{
-  const obj = createObject(arr);
-  return timesRepeat(obj,Object.keys(obj));
+const createObject = (arr)=>{
+  return 0;
 }
 
 module.exports = {
